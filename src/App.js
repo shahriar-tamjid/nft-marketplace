db25724ee450550f325b9d97a6829d6f6d7518f6
Shahriar Tamjid <65736523+shahriar-tamjid@users.noreply.github.com>
2023-02-03 23:36:59 +0600

    Created NFT Marketplace Website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Choose from "./components/Choose";
+import Clients from "./components/Clients";
+import CreateAndSell from "./components/CreateAndSell";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Marketplace from "./components/Marketplace";
+import ScrollToTop from "./components/ScrollToTop";
+import Subscribe from "./components/Subscribe";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Home />
+      <Clients />
+      <CreateAndSell />
+      <Choose />
+      <Marketplace />
+      <Subscribe />
+      <Footer />
+      <ScrollToTop />
     </div>
   );
-}
+};
 
 export default App;
